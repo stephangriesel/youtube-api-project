95f54ac0d964d5d5725db6467972343fcd5ed4eb
Stephan Griesel <sgriesel@gmail.com>
2018-07-18 06:25:54 +0200

    start js

---
+++ script.js
@@ -0,0 +1,25 @@
+// Options
+const CLIENT_ID = ''; //client ID, hide from repository
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
+const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
+
+const authorizeButton = document.getElementById('login-button');
+const signoutButton = document.getElementById('logout-button');
+const content = document.getElementById('content');
+const channelForm = document.getElementById('channel-form');
+const channelInput = document.getElementById('channel-input');
+const videoContainer = document.getElementById('video-container');
+
+// Load auth2 library
+function handleClientLoad() {
+    gapi.load('client:auth2', initClient);
+}
+
+// Init API client library and set up sign in listeners
+function initClient() {
+    gapi.client.init({
+        discoveryDocs: DISCOVERY_DOCS,
+        clientId: CLIENT_ID,
+        scope: SCOPES
+    });
+}
